4183a18ff4985a173244b597bbbedbc21c986c69
mehmetcan kul <138331728+McanKul@users.noreply.github.com>
2025-06-13 15:16:30 +0300

    Updated StatementsSection.tsx

--- src/components/sections/StatementsSection.tsx
+++ src/components/sections/StatementsSection.tsx
@@ -224,18 +224,6 @@ const StatementsSection = () => {
 
       return (
         <div className="bg-white rounded-lg border border-gray-200">
-          {/* Banking Alert */}
-          <div className="p-6 border-b border-gray-200">
-            <div className="bg-red-50 border border-red-200 rounded-lg p-4 flex items-center space-x-3">
-              <AlertTriangle size={20} className="text-red-500" />
-              <p className="text-red-700">
-                Please complete filling out your{' '}
-                <a href="#" className="text-blue-600 hover:underline">
-                  Banking information
-                </a>
-              </p>
-            </div>
-          </div>
 
           {/* Date Range Selector */}
           <div className="p-6 border-b border-gray-200">
